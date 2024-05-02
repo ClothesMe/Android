a6e61dd6b38bf302e0756cb614d6a95e5c45f21b
jeonghee-Han <134862850+Jeonghee-Han@users.noreply.github.com>
2024-05-03 01:01:48 +0900

    Init Commit

---
+++ app/src/main/java/com/example/clothesme_android/ClothesMeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.clothesme_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClothesMeApplication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

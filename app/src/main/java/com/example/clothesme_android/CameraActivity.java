fd786016aa0daddeb283d5d5e3c175b21867ad5c
jeonghee-Han <134862850+Jeonghee-Han@users.noreply.github.com>
2024-05-22 12:13:35 +0900

    remove: 불필요한 코드 및 주석 제거

--- app/src/main/java/com/example/clothesme_android/CameraActivity.java
+++ app/src/main/java/com/example/clothesme_android/CameraActivity.java
@@ -12,23 +12,17 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.LifecycleCameraController;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.example.clothesme_android.databinding.ActivityCameraBinding;
 import com.google.common.util.concurrent.ListenableFuture;
 
-import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -43,7 +37,6 @@ public class CameraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         viewBinding = ActivityCameraBinding.inflate(getLayoutInflater());
         setContentView(viewBinding.getRoot());
-        LifecycleCameraController cameraController = new LifecycleCameraController(this);
 
         // 카메라 사용 권한
         if (allPermissionsGranted()) {
@@ -58,38 +51,32 @@ public class CameraActivity extends AppCompatActivity {
     }
 
     private void takePhoto() {
-        // ImageCapture UseCase에 대한 참조를 가져온다. 만약 초기화되지 않았다면 함수를 종료.
-        // UseCase는 이미지 캡처가 설정되기 전에 사진 버튼을 탭하면 null 된다.
         ImageCapture imageCapture = this.imageCapture;
         if (imageCapture == null) {
             return;
         }
 
-        // MediaStore 콘텐츠 값을 만든다.
-        // MediaStore의 표시 이름이 고유하도록 현재 시간을 기준으로 타임스탬프를 사용한다.
+        // MediaStore의 표시 이름이 고유하도록 현재 시간을 기준으로 타임스탬프를 사용
         String name = new SimpleDateFormat(FILENAME_FORMAT, Locale.US)
                 .format(new Date());
 
-        // ContentValues를 사용하여 이미지에 대한 메타데이터 설정
         ContentValues contentValues = new ContentValues();
         contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
         contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
         // Android Q 이상에서는 RELATIVE_PATH를 사용하여 저장 경로 지정
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
             contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
         }
 
-        // OutputFileOptions 객체를 생성.
-        // 이미지 저장 옵션 설정. MediaStore를 통해 이미지 저장 위치와 메타데이터 지정
-        // 이 객체에서 원하는 출력 방법을 지정할 수 있다.
+        // 이미지 저장 위치와 메타데이터 지정
+        // 이 객체에서 원하는 출력 방법을 지정
         ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions
                 .Builder(getContentResolver(),
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 contentValues)
                 .build();
 
-        // takePicture()를 호출한다. 이미지 캡처 및 저장
-        // outputOptions, 실행자, 이미지가 저장될 때 콜백을 전달
         imageCapture.takePicture(
                 outputOptions,
                 ContextCompat.getMainExecutor(this),
@@ -99,7 +86,6 @@ public class CameraActivity extends AppCompatActivity {
                         Log.e(TAG, "Photo capture failed: " + exc.getMessage(), exc);
                     }
 
-                    // 캡처에 실패하지 않으면 사진을 저장하고 완료되었다는 토스트 메시지를 표시한다.
                     @Override
                     public void onImageSaved(@NonNull ImageCapture.OutputFileResults output) {
                         String msg = "Photo capture succeeded: " + output.getSavedUri();
@@ -111,16 +97,8 @@ public class CameraActivity extends AppCompatActivity {
     }
 
     private void startCamera() {
-        // ProcessCameraProvider 인스턴스를 생성한다.
-        // 카메라의 수명 주기를 수명 주기 소유자와 바인딩하는 데 사용된다.
-        // CameraX가 수명 주기를 인식하므로 카메라를 열고 닫는 작업이 필요하지 않게 된다.
         ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-
-        // cameraProviderFuture에 리스너를 추가한다.
-        // 첫 번째 인수에는 Runnable를 넣는다.
-        // 두 번째는 기본 스레드에서 실행되는 Executor를 넣는다.
         cameraProviderFuture.addListener(() -> {
-            // 카메라 수명 주기를 애플리케이션 프로세스 내의 LifecycleOwner에 바인딩한다.
             ProcessCameraProvider cameraProvider = null;
             try {
                 cameraProvider = cameraProviderFuture.get();
@@ -128,19 +106,13 @@ public class CameraActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            // Preview 객체를 초기화 하고 뷰파인더에서 노출 영역 제공자를 가져온 다음 Preview에서 설정
             Preview preview = new Preview.Builder()
                     .build();
             preview.setSurfaceProvider(viewBinding.viewFinder.getSurfaceProvider());
 
-            // imageCaputre 인스턴스를 빌드
             imageCapture = new ImageCapture.Builder().build();
-
-            // 후면 카메라를 선택하는 객체를 생성
             CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
 
-            // cameraProvider에 바인딩된 항목이 없도록 한 다음
-            // 위에서 생성한 객체들을 cameraProvider에 바인딩
             try {
                 cameraProvider.unbindAll();
                 cameraProvider.bindToLifecycle(
@@ -187,39 +159,5 @@ public class CameraActivity extends AppCompatActivity {
                 finish();
             }
         }
-    }
-
-    private static class LuminosityAnalyzer implements ImageAnalysis.Analyzer {
-        private final LumaListener listener;
-
-        LuminosityAnalyzer(LumaListener listener) {
-            this.listener = listener;
-        }
-
-        private byte[] toByteArray(ByteBuffer buffer) {
-            buffer.rewind();    // Rewind the buffer to zero
-            byte[] data = new byte[buffer.remaining()];
-            buffer.get(data);   // Copy the buffer into a byte array
-            return data; // Return the byte array
-        }
-
-        @Override
-        public void analyze(ImageProxy image) {
-            ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-            byte[] data = toByteArray(buffer);
-            List<Integer> pixels = new ArrayList<>();
-            for (byte datum : data) {
-                pixels.add(datum & 0xFF);
-            }
-            double luma = pixels.stream().mapToInt(Integer::intValue).average().orElse(0.0);
-
-            listener.onLumaAvailable(luma);
-
-            image.close();
-        }
-    }
-
-    interface LumaListener {
-        void onLumaAvailable(double luma);
     }
 }
